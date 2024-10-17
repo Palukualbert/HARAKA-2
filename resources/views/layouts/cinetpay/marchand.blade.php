f3a8171422642dcba38d73458ab9dcf46f4f5e44
Albert Paluku <20pm500@esisalama.org>
2024-10-17 23:49:18 +0100

    Ajout de la fonction payment dans PaiementController

--- resources/views/layouts/cinetpay/marchand.blade.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$marchand = array(
-    "apikey" => "102883935766b9fc43834525.17240308", // Enrer votre apikey
-    "site_id" => "5877723", //Entrer votre site_ID
-    "secret_key" => "203624009966b9fce732a728.96649891" //Entrer votre clé secret
-);
-
